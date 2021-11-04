052127ec0ca9971f2a5572815e3b9f6bb8f214b1
yan <02480926001>
2021-11-04 17:32:38 -0300

    ...

--- accounts/views.py
+++ accounts/views.py
@@ -30,54 +30,51 @@ def logout(request):
 
 
 def cadastro(request):
-    # if request.method != 'POST':
-    return render(request, 'accounts/cadastro.html')
-    # #     print('nada postado')
-    #
-    # email = request.POST.get('email')
-    # usuario = request.POST.get('usuario')
-    # senha = request.POST.get('senha')
-    # senha2 = request.POST.get('senha2')
-    #
-    # if not usuario or not email or not senha or not senha2:
-    #     print('Nenhum campo pode ficar vazio')
-    #     return render(request, 'accounts/cadastro.html')
-    #
-    # try:
-    #     validate_email(email)
-    #
-    # except:
-    #     print('Email invalido')
-    #     return render(request, 'accounts/cadastro.html')
-    #
-    # if len(senha) < 6:
-    #     print('A senha precisa ter no mínimo 6 caracteres')
-    #     return render(request, 'accounts/cadastro.html')
-    #
-    # if len(usuario) < 3:
-    #     print('O usuario precisa ter no mínimo 3 caracteres')
-    #     return render(request, 'accounts/cadastro.html')
-    #
-    # if senha != senha2:
-    #     print('As senhas não são iguais')
-    #     return render(request, 'accounts/cadastro.html')
-    #
-    # if User.objects.filter(username=usuario).exists():
-    #     print('O usuario já esta cadastrado')
-    #     return render(request, 'accounts/cadastro.html')
-    #
-    # if User.objects.filter(email=email).exists():
-    #     print('E-mail já esta cadastrado')
-    #     return render(request, 'accounts/cadastro.html')
-    #
-    #
-    # user = User.objects.create_user(username=usuario, email=email, password=senha)
-    # user.save()
-    # print('Rgistrado com sucesso')
-    # return render(request, 'paginas/index.html')
+    if request.method != 'POST':
+        return render(request, 'accounts/cadastro.html')
+    #     print('nada postado')
+
+    email = request.POST.get('email')
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    senha2 = request.POST.get('senha2')
+
+    if not usuario or not email or not senha or not senha2:
+        print('Nenhum campo pode ficar vazio')
+        return render(request, 'accounts/cadastro.html')
+
+    try:
+        validate_email(email)
+
+    except:
+        print('Email invalido')
+        return render(request, 'accounts/cadastro.html')
+
+    if len(senha) < 6:
+        print('A senha precisa ter no mínimo 6 caracteres')
+        return render(request, 'accounts/cadastro.html')
+
+    if len(usuario) < 3:
+        print('O usuario precisa ter no mínimo 3 caracteres')
+        return render(request, 'accounts/cadastro.html')
+
+    if senha != senha2:
+        print('As senhas não são iguais')
+        return render(request, 'accounts/cadastro.html')
+
+    if User.objects.filter(username=usuario).exists():
+        print('O usuario já esta cadastrado')
+        return render(request, 'accounts/cadastro.html')
+
+    if User.objects.filter(email=email).exists():
+        print('E-mail já esta cadastrado')
+        return render(request, 'accounts/cadastro.html')
 
 
-
+    user = User.objects.create_user(username=usuario, email=email, password=senha)
+    user.save()
+    print('Rgistrado com sucesso')
+    return render(request, 'accounts/login.html')
 
 
 @login_required(redirect_field_name='login')
---
+++ readme.md
@@ -0,0 +1,11 @@
+##  Livraria de Classe
+   Este projeto esta sendo desenvolvido para a disciplina de programação 
+  orientada a objeto. 
+  
+###Instalação
+>   -> python -m venv venv  
+    ->venv\Scripts\activate.bat  
+    -> python -m pip install --upgrade pip setuptools wheel --user  
+    -> python -m pip install django django-debug-toolbar django-crispy-forms
+pillow  
+    ->python manage.py migrate  
