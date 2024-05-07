c02c87c1aea80ecf5a44f7c5c2e1b9c231d4aca2
Yan-Magnan <yanmagnan144@gmail.com>
2024-05-07 08:30:26 -0300

    refactory for new system pet rescue

--- paginas/admin.py
+++ paginas/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Categoria, Livros
+from .models import Categoria, Livros, pet
 class LivrosAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'editora', 'ano_Lancamento', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome')
     list_per_page = 5
 
 admin.site.register(Categoria)
+admin.site.register(pet)
 admin.site.register(Livros, LivrosAdmin)
 # admin.site.register(MeusLivro)
 
--- paginas/models.py
+++ paginas/models.py
@@ -23,6 +23,35 @@ class Livros(models.Model):
     def __str__(self):
         return self.nome
 
+class Livros2(models.Model):
+    nome = models.CharField(max_length=80)
+    editora = models.CharField(max_length=80)
+    descricao = models.TextField(blank=True)
+    ano_Lancamento = models.DateTimeField(default=timezone.now)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    mostrar = models.BooleanField(default=True)
+    foto = models.ImageField(blank=True, upload_to='fotos/%y/%m/')
+    livro = models.FileField(blank=True, upload_to='pdfs/%y/%m/')
+    # arquivo = models.FilePathField(blank=False)
+
+    def __str__(self):
+        return self.nome
+class pet(models.Model):
+    status = models.CharField(max_length=80)
+    foto = models.ImageField(blank=True, upload_to='fotos/%y/%m/')
+    nome = models.CharField(max_length=80)
+    raca = models.CharField(max_length=80)
+    tutor = models.CharField(max_length=180)
+    email = models.CharField(max_length=260)
+    telefone = models.CharField(max_length=80)
+    cpf = models.CharField(max_length=260)
+    endereco = models.CharField(max_length=150)
+    data_acolhimento = models.DateTimeField(default=timezone.now)
+    historico_medico = models.CharField(max_length=500000, blank=True)
+
+
+    def __str__(self):
+        return self.nome
 
 
 
