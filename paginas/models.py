3e401c35bfa9bc95745abef35c3b86b48830a0eb
Jonathan Dutra Hartmann <120300587+J00naTHan@users.noreply.github.com>
2024-05-07 13:19:14 -0300

    Update models.py

--- paginas/models.py
+++ paginas/models.py
@@ -8,7 +8,6 @@ class Categoria(models.Model):
         return self.nome
 
 
-
 class Livros(models.Model):
     STATUS_CHOICES = [
         ('Tutor não localizado', 'Tutor não localizado'),
@@ -33,47 +32,3 @@ class Livros(models.Model):
 
     def __str__(self):
         return self.nome
-class pet(models.Model):
-    status = models.CharField(max_length=80)
-    foto = models.ImageField(blank=True, upload_to='fotos/%y/%m/')
-    nome = models.CharField(max_length=80)
-    raca = models.CharField(max_length=80)
-    tutor = models.CharField(max_length=180)
-    email = models.CharField(max_length=260)
-    telefone = models.CharField(max_length=80)
-    cpf = models.CharField(max_length=260)
-    endereco = models.CharField(max_length=150)
-    data_acolhimento = models.DateTimeField(default=timezone.now)
-    historico_medico = models.CharField(max_length=500000, blank=True)
-
-class Livros2(models.Model):
-
-    nome = models.CharField(max_length=80)
-    editora = models.CharField(max_length=80)
-    descricao = models.TextField(blank=True)
-    ano_Lancamento = models.DateTimeField(default=timezone.now)
-    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
-    mostrar = models.BooleanField(default=True)
-    foto = models.ImageField(blank=True, upload_to='fotos/%y/%m/')
-    livro = models.FileField(blank=True, upload_to='pdfs/%y/%m/')
-
-
-
-
-    # arquivo = models.FilePathField(blank=False)
-
-    def __str__(self):
-        return self.nome
-
-
-
-    def __str__(self):
-        return self.nome
-
-
-
-
-
-
-
-
