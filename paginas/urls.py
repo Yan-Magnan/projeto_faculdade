c8c08763c84d29d44a75479e3db6525a0984ba59
Jonathan Dutra Hartmann <120300587+J00naTHan@users.noreply.github.com>
2024-05-09 12:23:21 -0300

    Update urls.py

--- paginas/urls.py
+++ paginas/urls.py
@@ -2,13 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
     path('', views.index, name='index'),
     path('<int:livros_id>', views.abre_livro, name='abre_livro'),
     path('buscar/', views.buscar, name='buscar'),
     path('profile/', views.profile, name='profile'),
     path('ver_livros/', views.ver_livros, name='ver_livros'),
-    path('lista_desejo/', views.lista_desejo, name='lista_desejo'),
-    path('minha_colecao/', views.minha_colecao, name='minha_colecao'),
-
 ]
