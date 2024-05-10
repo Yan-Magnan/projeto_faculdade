d4522e846d375a7cb3b24021a5405567de67c593
Yan-Magnan <yanmagnan144@gmail.com>
2024-05-10 16:12:44 -0300

    Filtro funcional

--- paginas/views.py
+++ paginas/views.py
@@ -1,49 +1,45 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Pets, Categoria  # Substituído Livros por Pets
-from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib import messages
-from django.contrib.auth.models import User
+from .models import Pets, Categoria
 from django.contrib.auth.decorators import login_required
 
-# @login_required(redirect_field_name='login')
+
+
 def index(request):
-    pets = Pets.objects.all()  # Substituído Livros por Pets
-    return render(request, 'paginas/index.html', {
-        'pets': pets
-    })
+    pets = Pets.objects.all()
 
-# @login_required(redirect_field_name='login')
+    especie = request.GET.get('especie')
+    raca = request.GET.get('raca')
+    porte = request.GET.get('porte')
+    sex = request.GET.get('sex')
+
+    if especie:
+        pets = pets.filter(especie=especie)
+    if raca:
+        pets = pets.filter(raca=raca)
+    if porte:
+        pets = pets.filter(porte=porte)
+    if sex:
+        pets = pets.filter(sex=sex)
+
+    return render(request, 'paginas/index.html', {'pets': pets})
+
+
+
 def ver_pet(request, pets_id):
-    pet = get_object_or_404(Pets, id=pets_id)  # Substituído Livros por Pets
-    return render(request, 'paginas/ver_pet.html', {
-        'pet': pet
-    })
+    pet = get_object_or_404(Pets, id=pets_id)
+    return render(request, 'paginas/ver_pet.html', {'pet': pet})
 
-# @login_required(redirect_field_name='login')
+
+
 def profile(request):
     return render(request, 'paginas/profile.html')
 
-# @login_required(redirect_field_name='login')
-# def ver_pet(request):
-#     pets = Pets.objects.all()  # Substituído Livros por Pets
-#     return render(request, 'paginas/ver_pet.html', {
-#         'pets': pets,
-#     })
 
-# @login_required(redirect_field_name='login')
+
 def lista_desejo(request):
     return render(request, 'paginas/lista_desejo.html')
 
-# @login_required(redirect_field_name='login')
+
+
 def minha_colecao(request):
     return render(request, 'paginas/minha_colecao.html')
-
-# @login_required(redirect_field_name='login')
-def buscar(request):
-    search = request.GET.get('search')
-    pets = Pets.objects.filter(categoria__nome=search)  # Substituído Livros por Pets
-    return render(request, 'paginas/buscar.html', {
-        'pets': pets,
-    })
-
-
